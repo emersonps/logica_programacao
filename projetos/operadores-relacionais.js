0bb1acacda8339c944e13a4c58479f7db9bf4356
emersonline2007 <emersonline2007@gmail.com>
2020-06-18 21:23:54 -0400

    organização

--- projetos/operadores-relacionais.js
+++
@@ -1,9 +0,0 @@
-var nota01, nota02;
-
-nota01 = 10;
-nota02 = 15;
-
-if(nota01 != nota02)
-    alert("Nota01 é diferente de nota02");
-else
-    alert("Nota01 é igual a nota01");
--- projetos/primeiro.js
+++
@@ -1,4 +0,0 @@
-var nota, nota1, nome, media, primeiroNome, segundoNome, endereco;
-nota = 100;
-alert("Resultado: " + nota);
-
